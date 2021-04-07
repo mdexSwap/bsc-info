8f2e397b680b69e221795c2adf065dd0a776154d
Mdex <76643605+mdexSwap@users.noreply.github.com>
2021-04-08 02:39:42 +0800

    Add files via upload

---
+++ precache-manifest.7e2b07b35b99d74176311efb548cec03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690d04af96011b8bf92dfb1450cfffbb",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/index.html"
+  },
+  {
+    "revision": "8dd53cdba749d582a466",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/js/2.c8fc326d.chunk.js"
+  },
+  {
+    "revision": "627773ea9d4d22328b7e3c164cff9ea5",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/js/2.c8fc326d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f8c5cc3f3a401550988",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/js/main.33472667.chunk.js"
+  },
+  {
+    "revision": "efe2cf28cf6e8b256b52",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/js/runtime-main.f930dac4.js"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/media/logo.8e72f6f2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/precache-manifest.112b66b1dc29d26fd700e374867e634c.js"
+  "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/precache-manifest.7e2b07b35b99d74176311efb548cec03.js"
 );
 
 self.addEventListener('message', (event) => {

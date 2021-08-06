7875e31afdad88461e218dae1bfaa24dbd154d0b
mdexswap <mdexswap@protonmail.com>
2021-08-06 23:41:54 +0800

    update

---
+++ precache-manifest.617a3449d0bf572e5679c9a74ca50b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d202942d7ba43e4957754d98ddee9e5",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/index.html"
+  },
+  {
+    "revision": "34c0999c0340120c8a90",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/js/2.148b093f.chunk.js"
+  },
+  {
+    "revision": "627773ea9d4d22328b7e3c164cff9ea5",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/js/2.148b093f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e4a63387bad9b742c3",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/js/main.2c309f8e.chunk.js"
+  },
+  {
+    "revision": "efe2cf28cf6e8b256b52",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/js/runtime-main.f930dac4.js"
+  },
+  {
+    "revision": "8e72f6f2edbc451e3bba41e50d2b8fcd",
+    "url": "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/static/media/logo.8e72f6f2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/precache-manifest.7be7c1707cfe41d278874b35f5a7a329.js"
+  "https://cdn.jsdelivr.net/gh/mdexSwap/bsc-info@main/precache-manifest.617a3449d0bf572e5679c9a74ca50b1d.js"
 );
 
 self.addEventListener('message', (event) => {
